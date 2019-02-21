56bf234f91232104e0880e5d4ee096cb43c39c7a
cghaase <cghaase@gmail.com>
2019-02-21 13:39:59 -0700

    testing for vignettes

---
+++ vignettes/GARPTools_vignette.R
@@ -0,0 +1,11 @@
+## ----message = FALSE, warning = FALSE, comment = "R>"--------------------
+library(GARPTools)
+
+## ----comment = "R>"------------------------------------------------------
+data("wtdeer_df")
+head(wtdeer_df)
+
+## ----comment = "R>"------------------------------------------------------
+data("wtdeer_locations")
+wtdeer_locations
+
