9e41393f267c2edb88228534eddb306552caa171
cghaase <cghaase@gmail.com>
2019-02-23 13:36:13 -0700

    fixing vignette

--- vignettes/GARPTools_vignette.R
+++ vignettes/GARPTools_vignette.R
@@ -9,3 +9,19 @@ head(wtdeer_df)
 data("wtdeer_locations")
 wtdeer_locations
 
+## ----eval = TRUE, echo = FALSE-------------------------------------------
+data(alt)
+data(bio_1)
+data(bio_12)
+
+
+## ----eval = FALSE, echo = TRUE-------------------------------------------
+#  files <- list.files(path=paste(system.file(package="GARPTools"),"/data", sep = ""),
+#                      pattern = ".asc", full.names = TRUE)
+#  
+#  env_layers <- stack(files)
+#  
+#  writeRaster(env_layers[[1]], "C:/GARP/rasters/alt.asc", overwrite=TRUE)
+#  writeRaster(env_layers[[2]], "C:/GARP/rasters/bio_1.asc", overwrite=TRUE)
+#  writeRaster(env_layers[[3]], "C:/GARP/rasters/bio_12.asc", overwrite=TRUE)
+
